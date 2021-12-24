d203aee0c61476a2b98b353600ec8763c50f6afe
nikiten <nikitenuss@mail.ru>
2021-12-24 19:39:18 +0300

    props to the message component

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import Message from "./Message";
+
+export type messageDataType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
@@ -15,12 +23,12 @@ function HW1() {
 
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {messageDataType} from "./HW1";
+import s from './Message.module.css'
 
-function Message() {
+function Message(props: messageDataType) {
     return (
-        <div>
-
+        <div className={s.shell}>
+          <div className={s.message}>
+              <div><img className={s.ava} src={props.avatar} alt="ava"/></div>
+              <div className={s.dialog}>
+                  <div>{props.name}</div>
+                  <div>{props.message}</div>
+                  <div>{props.time}</div>
+              </div>
+          </div>
         </div>
     )
 }
