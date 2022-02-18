94256000ee6647c38766b15e0c1c70952d43dbf7
Vlad <nikitenuss@mail.ru>
2022-02-18 20:52:21 +0300

    first project

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -4,15 +4,15 @@ import s from './Message.module.css'
 
 function Message(props: messageDataType) {
     return (
-        <div className={s.shell}>
-          <div className={s.message}>
-              <div><img className={s.ava} src={props.avatar} alt="ava"/></div>
-              <div className={s.dialog}>
-                  <div>{props.name}</div>
-                  <div>{props.message}</div>
-                  <div>{props.time}</div>
-              </div>
-          </div>
+        <div className={s.message}>
+            <div><img className={s.ava} src={props.avatar} alt="ava"/></div>
+            <div className={s.dialog}>
+                <div className={s.dialogData}>
+                    <div>{props.name}</div>
+                    <div>{props.message}</div>
+                </div>
+                <div className={s.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
